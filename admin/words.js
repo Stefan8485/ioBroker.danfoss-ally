e4113e1e913cedc3fdc2df66867a08669b25a394
Stefan <152068509+Stefan8485@users.noreply.github.com>
2025-10-06 14:26:04 +0200

    Update words.js

--- admin/words.js
+++ admin/words.js
@@ -1,9 +1,9 @@
 systemDictionary = {
+  "Danfoss Ally Cloud Adapter": { en: "Danfoss Ally Cloud Adapter", de: "Danfoss Ally Cloud Adapter" },
   "API Key (Client ID)": { en: "API Key (Client ID)", de: "API-Schlüssel (Client-ID)" },
   "API Secret (Client Secret)": { en: "API Secret (Client Secret)", de: "API-Geheimnis (Client-Secret)" },
   "Token URL (OAuth2)": { en: "Token URL (OAuth2)", de: "Token-URL (OAuth2)" },
   "API Base URL": { en: "API Base URL", de: "API-Basis-URL" },
   "Scope (optional)": { en: "Scope (optional)", de: "Bereich (optional)" },
-  "Polling Interval (s)": { en: "Polling Interval (s)", de: "Abfrageintervall (s)" },
-  "Danfoss Ally Cloud Adapter": { en: "Danfoss Ally Cloud Adapter", de: "Danfoss Ally Cloud Adapter" }
+  "Polling Interval (s)": { en: "Polling Interval (s)", de: "Abfrageintervall (s)" }
 };
